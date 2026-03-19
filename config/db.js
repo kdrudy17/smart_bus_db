e5539ccbc3d2b5c8efe5fc9cc6ea4dce4f409b97
kdrudy17 <darrenrfk17@gmail.com>
2026-03-19 10:24:02 +0100

    Remove dotenv.config() for Vercel deployment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 import mysql from "mysql2/promise";
-import dotenv from 'dotenv';
-dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -10,4 +8,3 @@ const pool = mysql.createPool({
 });
 
 export default pool;
-
