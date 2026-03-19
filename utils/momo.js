ae3dbda7033a3d3f8826fe5ef06765512025fa9b
kdrudy17 <darrenrfk17@gmail.com>
2026-03-19 10:19:43 +0100

    Update utils/momo.js with new access token function

--- utils/momo.js
+++ utils/momo.js
@@ -1,18 +1,36 @@
-// src/utils/momo.js
+// utils/momo.js
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const { MOMO_CONSUMER_KEY, MOMO_API_USER , MOMO_API_KEY , MOMO_BASE_URL } = process.env;
+const { MOMO_CONSUMER_KEY, MOMO_API_USER, MOMO_API_KEY, MOMO_BASE_URL } = process.env;
 
 // Get access token
-export async function getAccessToken() { 
-  const credentials = Buffer.from(`${MOMO_API_USER}:${MOMO_API_KEY}`).toString("base64"); 
-  try { 
-    const response = await axios.post(`${MOMO_BASE_URL}/collection/token/`, null,
-      { 
-        headers: { Authorization: `Basic ${credentials}`, "Ocp-Apim-Subscription-Key": MOMO_CONSUMER_KEY, }, }); 
-        console.log("🔑 Access Token:", response.data.access_token); 
-    return response.data.access_token; } 
-  catch (err) { console.error("❌ Error getting MoMo token:", err.response?.data || err.message); throw err; } }
+export async function getAccessToken() {
+  try {
+    const credentials = Buffer.from(
+      `${MOMO_API_USER}:${MOMO_API_KEY}`
+    ).toString("base64");
+
+    const response = await axios.post(
+      `${MOMO_BASE_URL}/collection/token/`,
+      null,
+      {
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          "Ocp-Apim-Subscription-Key": MOMO_CONSUMER_KEY,
+        },
+      }
+    );
+
+    if (process.env.NODE_ENV !== "production") {
+      console.log("🔑 Access Token:", response.data.access_token);
+    }
+
+    return response.data.access_token;
+  } catch (err) {
+    console.error(
+      "❌ Error getting MoMo token:",
+      err.response?.data || err.message
+    );
+    throw err;
+  }
+}
